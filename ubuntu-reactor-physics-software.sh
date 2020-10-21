e16d7a8c29fb6708744a6b21702c625aabbfbc1c
yardasol <45364492+yardasol@users.noreply.github.com>
2020-10-21 13:38:35 -0500

    Cleaned up the code, moved all installations to conda

--- ubuntu-reactor-physics-software.sh
+++ ubuntu-reactor-physics-software.sh
@@ -8,30 +8,14 @@ sudo apt-get install update
 sudo apt-get install make -y
 sudo apt-get install update
 
-#install Miniconda3
-#curl -L -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-#bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
-
-#Export miniconda3 to your PATH
-#export PATH=$HOME/miniconda3/bin:$PATH
-
 #Configure conda to work with conda-forge 
 conda config --add channels conda-forge
 
 #add the MOOSE framework channel
 conda config --add channels idaholab
 
-#Create nuclear environment and install the OpenMC, MOOSE, and yt packages
-conda create --name nuclear openmc moose-libmesh moose-tools yt tox 
-
-#install gmsh
-pip install --upgrade gmsh
-
-#install pip3
-sudo apt-get install python3-pip
-
-#Install tox
-pip install tox
+#Create nuclear environment and install the OpenMC, MOOSE, yt, and gmsh packages
+conda create --name nuclear openmc moose-libmesh moose-tools yt tox gmsh
 
 #create projects directory
 mkdir ~/projects
@@ -58,10 +42,3 @@ git submodule update
 
 #leave the nuclear environment
 conda deactivate
-
-#Setup ARMI
-#cd ~/projects/armi
-#pip3 install -r requirements.txt
-#python3 setup.py install
-#armi
-#pip3 install -r requirements-testing.txt
