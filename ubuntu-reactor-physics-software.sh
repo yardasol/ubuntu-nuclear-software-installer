fcd4d56f320eae21b7681c0b82b3a3e5ef8bfcfe
yardasol <yardasol@grinnell.edu>
2020-12-21 10:04:36 -0600

    Added g++ installation functionality

--- ubuntu-reactor-physics-software.sh
+++ ubuntu-reactor-physics-software.sh
@@ -1,47 +1,57 @@
 #! ~/bin/bash
 
-#install gcc
+# Install gcc
 sudo apt-get install gcc -y
 sudo apt-get install update
 
-#install make
+# Install g++
+sudo apt-get install g++ -y
+sudo apt-get install update
+
+# Install make
 sudo apt-get install make -y
 sudo apt-get install update
 
-#install cmake
+# Install cmake
 sudo apt-get install cmake -y
 sudo apt-get install update
 
-#Configure conda to work with conda-forge 
+# Configure conda to work with conda-forge 
 conda config --add channels conda-forge
 
-#add the MOOSE framework channel
+# Add the MOOSE framework channel
 conda config --add channels idaholab
 
-#Create nuclear environment and install the OpenMC, MOOSE, yt, gmsh, pyne packages
-conda create --name nuclear jupyter openmc moose-libmesh moose-tools yt gmsh pyne
+# Create nuclear environment and install the OpenMC, MOOSE, yt, gmsh, pyne packages
+conda create --name nuclear openmc pyne yt gmsh moose-libmesh moose-tools jupyter --yes
 
-#create projects directory
+# Create environements that have more recent versions of the software
+conda create --name moose-env moose-libmesh moose-tools --yes
+conda create --name openmc-env openmc pyne jupyter yt gmsh --yes
+
+# Create projects directory
 mkdir ~/projects
 cd ~/projects
 
-#install MOOSE, Moltres sourcecode
+# Install sources for nuclear software
 git clone https://github.com/idaholab/moose
 git clone https://github.com/arfc/moltres
+git clone https://github.com/openmc-dev/openmc
+git clone https://github.com/pyne/pyne
 
-#activate the nuclear environment
+# Activate the moove-env environment
 conda init bash
-conda activate nuclear
+conda activate moose-env
 
-#compile MOOSE
+# Compile MOOSE
 cd ~/projects/moose/test
 make -j 4
 
-#Initalize Moltres
+# Initalize Moltres
 cd ~/projects/moltres
 git submodule init
 git submodule update
 ./build_libmesh_and_moltres.sh
 
-#leave the nuclear environment
+# Leave the nuclear environment
 conda deactivate
