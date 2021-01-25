4306f9f90cf3a4b6fd9cbda72751cd89b245b9c6
yardasol <yardasol@grinnell.edu>
2021-01-25 13:10:51 -0600

    added line to change to ssh

--- temoa.sh
+++ temoa.sh
@@ -9,6 +9,7 @@ conda activate temoa-py3
 git clone https://github.com/TemoaProject/temoa
 git clone https://github.com/yardasol/pride
 cd pride
+git remote set-url origin git@github.com:yardasol/pride
 git remote add upstream https://github.com/arfc/pride
 git fetch upstream
 git merge upstream/master
