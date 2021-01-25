6efbfa99d625b87fc2f2a0dd34ad30649ddba2f9
yardasol <yardasol@grinnell.edu>
2021-01-25 14:47:08 -0600

    New file: the main one

---
+++ ubuntu-nuclear-software-installer.sh
@@ -0,0 +1,20 @@
+# Step 0: collect information from user
+
+# github username
+
+# github repository URLs
+
+# process repo URLs to get repo names
+
+# ask for user if they have forked repos
+
+# ask user where they want the repos installed
+
+# Step 1: install software, build tools, and all dependencies
+source install.sh
+
+# Step 2: build software from source
+source build.sh
+
+# Step 3: test that software works
+source test.sh
