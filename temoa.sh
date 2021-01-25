06c053b8e02645e50941cd4035e915d270044b1b
yardasol <yardasol@grinnell.edu>
2021-01-25 12:15:23 -0600

    added temoa scriptlet

---
+++ temoa.sh
@@ -0,0 +1,14 @@
+#! ~/bin/bash
+
+cd ~/projects
+mkdir temoa-py3; cd temoa-py3
+wget https://github.com/TemoaProject/temoa/blob/energysystem/environment.yml
+conda init bash
+conda env create
+conda activate temoa-py3
+git clone https://github.com/TemoaProject/temoa
+git clone https://github.com/yardasol/pride
+cd pride
+git remote add upstream https://github.com/arfc/pride
+git fetch upstream
+git merge upstream/master
