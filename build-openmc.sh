b9b264253fc44562190ba9442b22d9b3b5d3be2f
yardasol <yardasol@grinnell.edu>
2021-03-01 10:23:30 -0600

    cleaned up openmc build script

--- build-openmc.sh
+++ build-openmc.sh
@@ -15,16 +15,15 @@ conda install hdf5=1.10.4 h5py numpy scipy matplotlib uncertainties lxml mpi4py 
 
 # Build Openmc from source
 mkdir build app && cd build
-#CXX=mpicxx.openmpi HDF5_ROOT=/usr cmake -Ddebug=on -Dprofile=on -Doptimize=on -Dcoverage=on -DHDF5_PREFER_PARALLEL=on -DCMAKE_INSTALL_PREFIX=$NUC_SOFTWARE/openmc/app ..
-HDF5_ROOT=/home/ooblack/anaconda3/envs/openmc-env cmake -Ddebug=on -Doptimize=on -DCMAKE_INSTALL_PREFIX=$NUC_SOFTWARE/openmc/app ..
+CC=gcc \
+CXX=g++ \
+HDF5_ROOT=/home/ooblack/anaconda3/envs/openmc-env \
+cmake \ -Ddebug=on -Doptimize=on -DCMAKE_INSTALL_PREFIX=$HOME/projects/openmc/app ..
 make
 make install
 
-conda activate openmc-env
 cd ..
-#MPICC=mpicc.openmpi pip install --upgrade-strategy only-if-needed mpi4py
-#CC=mpicc.openmpi HDF5_MPI=ON HDF5_DIR=/usr pip install --upgrade-strategy only-if-needed --no-binary=h5py h5py
 HDF5_DIR=/home/ooblack/anaconda3/envs/openmc-env/bin pip install --upgrade-strategy only-if-needed --no-binary=h5py h5py
-pip install --upgrade-strategy only-if-needed -e .[test]
+python pip install --upgrade-strategy only-if-needed -e .[test]
 
 
