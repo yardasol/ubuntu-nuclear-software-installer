78f6f00995b474f31f5d458717281c6350d27be5
yardasol <oyardas2@illinos.edu>
2021-06-29 11:21:26 -0500

    minor typo fix

--- hdf5.sh
+++ hdf5.sh
@@ -4,7 +4,7 @@ VERSION=1.10
 SUBVERSION=4
 wget -q -O - https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-$VERSION/hdf5-$VERSION.$SUBVERSION/src/hdf5-$VERSION.$SUBVERSION.tar.gz | tar -C $HOME -zx
 
-HDF5_INSTALL_PATH=/user/local/hdf5-$VERSION
+HDF5_INSTALL_PATH=/usr/local/hdf5-$VERSION
 cd $HOME/hdf5-$VERSION.$SUBVERSION
 CC=gcc ./configure --prefix=$HDF5_INSTALL_PATH --enable-cxx
 make
