ef09b6bc4795d6e178536428138a14e5d3a52bbb
yardasol <oyardas2@illinos.edu>
2021-06-29 12:14:53 -0500

    updated install path

--- hdf5.sh
+++ hdf5.sh
@@ -1,10 +1,10 @@
 #! ~\bin\bash
 
 VERSION=1.10
-SUBVERSION=4
+SUBVERSION=7
 wget -q -O - https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-$VERSION/hdf5-$VERSION.$SUBVERSION/src/hdf5-$VERSION.$SUBVERSION.tar.gz | tar -C $HOME -zx
 
-HDF5_INSTALL_PATH=/usr/local/hdf5-$VERSION
+HDF5_INSTALL_PATH=/usr/local/hdf5-$VERSION.$SUBVERSION
 cd $HOME/hdf5-$VERSION.$SUBVERSION
 CC=gcc ./configure --prefix=$HDF5_INSTALL_PATH --enable-cxx
 make
