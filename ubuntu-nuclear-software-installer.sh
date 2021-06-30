55b807b86098e324f976e36b77fcd3cd1d0f88fc
yardasol <oyardas2@illinos.edu>
2021-06-30 14:57:26 -0500

    cleaned up file

--- ubuntu-nuclear-software-installer.sh
+++ ubuntu-nuclear-software-installer.sh
@@ -1,15 +1,3 @@
-# Step 0: collect information from user
-
-# github username
-
-# github repository URLs
-
-# process repo URLs to get repo names
-
-# ask for user if they have forked repos
-
-# ask user where they want the repos installed
-
 # Step 1: install software, build tools, and all dependencies
 source install.sh
 
