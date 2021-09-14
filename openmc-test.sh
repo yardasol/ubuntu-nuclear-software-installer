12fb28ef55a27b3752e37bd797b24656bd3fe98b
yardasol <yardasol@grinnell.edu>
2021-09-14 09:50:54 -0500

    Added line to return to execution directory

--- openmc-test.sh
+++ openmc-test.sh
@@ -1,5 +1,8 @@
 #! ~/bin/bash
 
+# Paths
+CURRENT=`pwd`
+
 # Initialize envirnoment variables for openmc
 export OPENMC_CROSS_SECTIONS=$HOME/nndc_hdf5/cross_sections.xml
 export OPENMC_ENDF_DATA=$HOME/endf-b-vii.1
@@ -9,3 +12,6 @@ export PATH=$HOME/projects/NJOY2016/bin:$PATH
 cd $HOME/projects/openmc/tests
 conda activate openmc-env
 pytest
+
+# Return to execution directory
+cd $CURRENT
