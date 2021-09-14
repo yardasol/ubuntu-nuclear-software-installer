a1d07b11c0c668fc6ccf2e7023a80731d5ec49d3
yardasol <yardasol@grinnell.edu>
2021-09-14 09:47:11 -0500

    add/modify comments

--- openmc-install.sh
+++ openmc-install.sh
@@ -10,10 +10,10 @@ if [[ ! -d "$PROJECTS" ]]; then
 	mkdir $PROJECTS
 fi
 
-#github username
+# github username
 GITHUB_UNAME=yardasol
 
-#Set up local git branch
+# Set up local git branch
 git clone git@github.com:$GITHUB_UNAME/openmc $HOME/projects/openmc
 cd $PROJECTS/openmc
 git remote add upstream git@github.com:openmc-dev/openmc
@@ -21,8 +21,7 @@ git fetch upstream
 git merge upstream/develop
 git push
 git checkout upstream/master
-git switch -c main
-git checkout main
+git switch -c master
 git branch --set-upstream-to=origin/master
 git merge upstream/master
 git push
@@ -37,6 +36,7 @@ fi
 
 # Download cross sections
 wget -O - https://anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz | tar -C $XSDIR -xJ
+
 # Add to PATH
 echo "export OPENMC_CROSS_SECTIONS="$XSDIR/endfb71_hdf5/cross_sections.xml"" >> $BRC
 source $HOME/.bashrc
