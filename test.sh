0eec3bb838fce66761bf2f94fce66f12518c2391
yardasol <yardasol@grinnell.edu>
2021-09-16 10:50:42 -0500

    added calls to pyne and saltproc pretesters and testers

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 # Test OpenMC
 source openmc-pretest.sh
 source openmc-test.sh
+
+# Test pyne
+source pyne-pretest.sh
+source pyne-test.sh
+
+# Test saltproc
+source saltproc-pretest.sh
+source saltproc-test.sh
