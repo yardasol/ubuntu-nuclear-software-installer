9cb786f72570c9cb20f202cf734155fe44d47209
yardasol <yardasol@grinnell.edu>
2021-10-11 17:11:17 -0500

    removed pyne related variables

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 # Install OpenMC
 source openmc-install.sh
 
-# Install pyne
-source pyne-install.sh
-
 # Install saltproc
 source saltproc-install.sh
--- test.sh
+++ test.sh
@@ -2,10 +2,6 @@
 source openmc-pretest.sh
 source openmc-test.sh
 
-# Test pyne
-source pyne-pretest.sh
-source pyne-test.sh
-
 # Test saltproc
 source saltproc-pretest.sh
 source saltproc-test.sh
