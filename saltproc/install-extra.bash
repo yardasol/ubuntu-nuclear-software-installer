9ec3dcac42eaffaf4dd6eda8e15750eab4b5dce9
yardasol <oyardas2@illinois.edu>
2022-03-23 11:30:28 -0500

    added extra script to install serpent cross sections

---
+++ saltproc/install-extra.bash
@@ -0,0 +1,14 @@
+XSDIR=$PROJECTS/cross-section-libraries
+
+# Make cross sections directory
+if [[ ! -d "$XSDIR" ]]; then
+	mkdir $XSDIR
+fi
+
+# Install cross sections
+source $SALTPROC_PATH/scripts/xsdir/process_j312.bash
+
+# Add .xsdata file to PATH (to be created)
+echo "export SERPENT_DATA="$XSDIR"" >> $BRC
+echo "export SERPENT_ACELIB="$XSDIR/jeff312/sss_jeff312.xsdata"" >> $BRC
+source $HOME/.bashrc
